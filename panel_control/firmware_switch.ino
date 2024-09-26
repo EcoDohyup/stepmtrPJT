d27021335763e4af9b4a51aff039c6e691f142d3
eco-bdh <ecobdh69@gmail.com>
2024-09-26 10:11:30 +0900

    switch code updated

--- panel_control/firmware_switch.ino
+++ panel_control/firmware_switch.ino
@@ -2,17 +2,13 @@ using namespace std;
 #define STEP_DIR_PIN 12
 #define STEP_STP_PIN 14
 #define STEP_ENA_PIN 13
-#define LIMIT_RIGHT = 4 // 우극한 위치
-#define LIMIT_LEFT = 2 // 좌극한 위치
+#define LIMIT_RIGHT 4 // 우극한 리미터 핀
+#define LIMIT_LEFT 2 // 좌극한 리미터 핀
 #define STEP_DELAY_MICROS 500 // 속도 아직 기존속도임
 
 // Variables to store motor state
 bool motorRunning = false; // 모터 운행상태 표시 변수
 int motorDirection = HIGH; // 기본 방향 우향으로 설정
-int motorPosition = 0; // 분주비, 사실상 위치값
-
-unsigned long lastPositionSendTime = 0; // 리미트 센서 쓰므로 안 쓸 확률 큼
-const unsigned long positionSendInterval = 500; // Interval in milliseconds
 
 void setup() {
   Serial.begin(115200);  // Initialize serial communication
@@ -23,6 +19,10 @@ void setup() {
   pinMode(STEP_STP_PIN, OUTPUT);
   pinMode(LIMIT_RIGHT, INPUT);
   pinMode(LIMIT_LEFT, INPUT);
+
+  digitalWrite(STEP_ENA_PIN, HIGH);
+  digitalWrite(STEP_DIR_PIN, motorDirection); // 기본 우향 세팅
+
   
 }
 
@@ -30,17 +30,47 @@ void loop() {
   // Read sensor value
   int limit_r = digitalRead(LIMIT_RIGHT);
   int limit_l = digitalRead(LIMIT_LEFT);
-  
-  // Debugging output: Print the sensor pin state
-  Serial.print("Sensor value: ");
-  Serial.println(sensorValue);
-  
-  // Check sensor state and print status
-  if (sensorValue == LOW) {
-    Serial.println("Object detected!");
-  } else {
-    Serial.println("No object detected.");
+
+  if (Serial.available() > 0) {
+    String command = Serial.readStringUntil('\n');
+    command.trim();
+    
+    if (command == "s") {
+      motorRunning = false;
+      digitalWrite(STEP_ENA_PIN, HIGH); // 모터 정지
+      Serial.println("모터 정지");
+    } 
+    else if (command == "d" && limit_r == HIGH) { // 우향
+      motorRunning = true;
+      motorDirection = HIGH; // 모터 우향 세팅
+      digitalWrite(STEP_ENA_PIN, LOW); // 모터 활성화
+      Serial.println("패널 개방중");
+    } 
+    else if (command == "a" && limit_l == HIGH) { // 좌향
+      motorRunning = true;
+      motorDirection = LOW; // 모터 좌향 세팅
+      digitalWrite(STEP_ENA_PIN, LOW); // 모터 활성화
+      Serial.println("패널 닫는중");
+    }
   }
-  
-  delay(500);  // Wait for 500 milliseconds before the next reading
+
+  if (motorRunning) {
+    digitalWrite(STEP_DIR_PIN, motorDirection); // 방향 조절
+    digitalWrite(STEP_STP_PIN, HIGH); // 분주비 제어
+    delayMicroseconds(STEP_DELAY_MICROS); // 펄스간 시간 조정, 고정됨
+    digitalWrite(STEP_STP_PIN, LOW); 
+    
+    if(limit_l == LOW && motorDirection == LOW){ // 좌한 정지
+      motorRunning = false; 
+      digitalWrite(STEP_ENA_PIN, HIGH); // 모터 완전 정지
+      Serial.println("패널 개방 완료");
+    } 
+    else if(limit_r == LOW && motorDirection == HIGH){ // 우한 정지
+      motorRunning = false; 
+      digitalWrite(STEP_ENA_PIN, HIGH);
+      Serial.println("패널 닫기 완료");
+    } 
+
+  }
+
 }
